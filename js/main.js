caf8eceb60e818c79a697686c754c1bdf5532e02
Parisa Shojaei <parisa.shojaei85@gmail.com>
2017-05-21 13:07:30 +0430

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
+function showTimer() {
+var endDate = new Date(document.getElementById("getDate").value).getTime();
+var updateDate = setInterval(function() {updateTimer()} ,1000);
+
+function updateTimer() {
+    var currentDate = new Date().getTime();
+    var distance = endDate - currentDate;
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+
+    if (distance <= 0) {
+       clearInterval(updateDate);
+       document.getElementById("showClock").innerHTML = "Expired";
+   }
+}
+}
+
 
